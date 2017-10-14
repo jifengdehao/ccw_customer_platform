7e5ddcb8ab84fd36f4766ba6a882992e89bbcb95
zhangwenlong <zhangwenlong@ccw163.com>
2017-10-14 10:19:01 +0800

    10:16 修改了axios返回约定 -zhangwenlong

--- config/config.js
+++ config/config.js
@@ -2,6 +2,6 @@ let config = {
   // env: Env,
   //apiDomain:'http://staffs.api.mingyi.loc',
   apiDomain: 'http://192.168.0.158:8093',
-  imgUpload: 'http://localhost:8082/upload'
+  imgUpload: ''
 };
 export default config
