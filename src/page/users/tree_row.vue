fc9e4d32c5c95a084b37e917fe80fa07a9a228e8
wufengliang <wufengliang@ccw163.com>
2017-11-16 11:08:48 +0800

    权限树修改

--- src/page/users/tree_row.vue
+++ src/page/users/tree_row.vue
@@ -42,6 +42,7 @@ export default {
     //  查看选中
     changeCheck(child, bool) {
       child.isHave = bool || !child.isHave
+      console.log('查看选中', child.isHave)
       if (!bool) {
         this.selectChildAll(child)
       }
@@ -70,7 +71,7 @@ export default {
     //  自定义权限点击
     checkPermission(permission, item, bool) {
       if (!this.parentData.childMenuList || !this.parentData.permissonList) {
-        item.isHave = !permission.isHave || true
+        item.isHave = true
         return
       }
       this.$emit('permissionCheck', {
@@ -82,13 +83,16 @@ export default {
     },
     //  权限点击
     permissionCheck(data) {
+      console.log(data, '权限点击')
       data.permission.isHave = !data.bool ? !data.permission.isHave : data.bool
-      data.item.isHave = data.permission.isHave
+      // data.item.isHave = data.permission.isHave
+      data.item.isHave = true
       this.checkPermission(data.item, data.parent, true)
     },
     //  向上传递值
     forwardChecked(data) {
       if (data.parent && data.isHave) {
+        console.log(data.isHave, 'forward')
         data.parent.isHave = true
         if (data.parent.permissonList) {
           data.parent.permissonList.forEach(item => {
