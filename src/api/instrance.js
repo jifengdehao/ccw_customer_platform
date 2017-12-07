3e6f65bb6ff9f12f58c9ccfc504970b5f8e56cd0
hushangjun <hsjcc@ccw163.com>
2017-12-07 17:40:29 +0800

    上线版本配置请求路径修改

--- src/api/instrance.js
+++ src/api/instrance.js
@@ -5,19 +5,12 @@
  * 功能模块: axios底层的封装
  */
 import axios from 'axios'
-import config from '../../config/config.js'
 import qs from 'qs'
 import iview from 'iview'
-// import * as ac from '../data/index.js'
 import hash from 'js-md5'
 
-// var URI = config.apiDomain
-var URI =
-  window.location.host.indexOf('localhost:') > -1
-    ? config.apiDomain
-    : 'http://192.168.0.151:8093/globalpt'
 var ax = axios.create({
-  baseURL: URI,
+  baseURL: process.env.BASE_API,
   timeout: 30000,
   withCredentials: true, // 跨域携带证书
   headers: {
@@ -74,8 +67,6 @@ export const pa = (url, params) => {
 export const op = (url, params) => {
   return base('options', url, params)
 }
-// 上传地址
-export const uploadUrl = config.imgUpload
 
 export function base (type, url, params) {
   return new Promise((resolve, reject) => {
